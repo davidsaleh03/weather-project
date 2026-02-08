1ecf0e9be894203b2ad99b59671d2ec67d2a8f97
David <davidsaleh03@gmail.com>
2026-02-08 02:36:42 -0800

    new

--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import NavTwo from '../components/NavTwo'
+import Cloudy from "../assets/cloudyicon.gif";
+import Humid from "../assets/raindrop.gif";
+import Rainy2 from "../assets/rainy2.gif";
+import Sunrise from "../assets/sunrise.gif";
+import Sunset from "../assets/sunset.gif";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
@@ -17,8 +22,154 @@ const Forecast = () => {
           </div>
         </div>
         <div className="info__data">
-            <div className="quality__more--module--1"></div>
-            <div className="quality__more--module--2"></div>
+            <div className="quality__more--module--1">
+              <div className="forecast__dates">
+                <h1 className="forecast__date-1">02-01</h1>
+                <h1 className="forecast__date-1">02-02</h1>
+                <h1 className="forecast__date-1">02-03</h1>
+                <h1 className="forecast__date-1">02-04</h1>
+                <h1 className="forecast__date-1">02-05</h1>
+                <h1 className="forecast__date-1">02-06</h1>
+                <h1 className="forecast__date-1">02-07</h1>
+              </div>
+            </div>
+            <div className="quality__more--module--2">
+              <div className="quality__more--module-top">
+                <div className="quality__more--module-1">
+                  <img src={Cloudy} alt="" className="forecast__img22" />
+                  <div className="quality__more--container22">
+                    <h1>High: 100°C</h1>
+                    <h1>Low: 100°C</h1>
+                  </div>
+                </div>
+                <div className="quality__more--module-1">
+                  <div className="forecast__humid2">
+                    <img src={Humid} alt="" />
+                    <h1 className="humid2__data">100%</h1>
+                  </div>
+                  <div className="forecast__humid2">
+                    <img src={Rainy2} alt="" />
+                    <h1 className="humid2__data">100%</h1>
+                  </div>
+                </div>
+                <div className="quality__more--module-1">
+                  <div className="forecast__condition--container">
+                    <h1 className="forecast__condition--h1">UV : 0.7</h1>
+                    <h1 className="forecast__condition--h1">CO : 370</h1>
+                    <h1 className="forecast__condition--h1">NO2 : 55</h1>
+                    <h1 className="forecast__condition--h1">O3 : 29</h1>
+                    <h1 className="forecast__condition--h1">SO2 : 18</h1>
+                  </div>
+                </div>
+                <div className="quality__more--module-1">
+                  <div className="forecast__humid2">
+                    <img src={Sunrise} alt="" />
+                    <h1 className="humid2__data">06:43</h1>
+                  </div>
+                  <div className="forecast__humid2">
+                    <img src={Sunset} alt="" />
+                    <h1 className="humid2__data">06:43</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="quality__more--module-bottom">
+                <div className="quality__more--module-2">
+                  <div className="forecast__hourly--container">
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                    <div className="forecast__hourly">
+                      <h1 className="forecast__hourly--title">10:00</h1>
+                      <div className="forecast__hourly--data">
+                        <h1>High: 100°C</h1>
+                        <h1>Low: 100°C</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
       </div>
     </div>
