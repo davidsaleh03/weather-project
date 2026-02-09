fee18167dfbf37b41552b55295f931213d4048cf
David <davidsaleh03@gmail.com>
2026-02-09 13:49:09 -0800

    here

--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -12,11 +12,12 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const Forecast = ( ) => {
+const Forecast = () => {
   const { city } = useParams();
   const [weatherData, setWeatherData] = useState("");
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
+  const [loading, setLoading] = useState(true);
+  const [activeIndex, setActiveIndex] = useState(0)
+  useEffect(() => {
     const getWeather = async () => {
       setLoading(true);
       try {
@@ -31,6 +32,12 @@ const Forecast = ( ) => {
     getWeather();
   }, [city]);
 
+  useEffect(() => {
+    if (weatherData?.forecast?.forecastday) {
+      setActiveIndex(0);
+    }
+  },[weatherData])
+
   if (loading) {
     return (
       <div className="loading-screen">
@@ -38,12 +45,19 @@ const Forecast = ( ) => {
       </div>
     );
   }
+
+  const activeDay = weatherData.forecast.forecastday[activeIndex]
+
   return (
     <div className="Forecast">
       <NavTwo />
       <div className="info__container">
         <div className="info__top">
-          <FontAwesomeIcon className="info__search" icon={faArrowLeft} />
+          <div className="info__top--left">
+                      <FontAwesomeIcon className="info__search" icon={faArrowLeft} />
+                      <h1>Forecast for "{weatherData.location.name},{" "}
+                      {weatherData.location.country}"</h1>
+                    </div>
           <div class="search__temp--change">
             <div class="temp__change--slider"></div>
             <button class="btn__temp-f click">°C</button>
@@ -53,13 +67,13 @@ const Forecast = ( ) => {
         <div className="info__data">
           <div className="quality__more--module--1">
             <div className="forecast__dates">
-              <h1 className="forecast__date-1">02-01</h1>
-              <h1 className="forecast__date-1">02-02</h1>
-              <h1 className="forecast__date-1">02-03</h1>
-              <h1 className="forecast__date-1">02-04</h1>
-              <h1 className="forecast__date-1">02-05</h1>
-              <h1 className="forecast__date-1">02-06</h1>
-              <h1 className="forecast__date-1">02-07</h1>
+              {weatherData.forecast.forecastday.map((date, index) => {
+                return (
+                  <h1 className={`forecast__date-1 ${activeIndex === index ? 'active' : ''}`} onClick={() => setActiveIndex(index)} key={index}>
+                    {date.date.slice(5, 10)}
+                  </h1>
+                );
+              })}
             </div>
           </div>
           <div className="quality__more--module--2">
@@ -67,106 +81,71 @@ const Forecast = ( ) => {
               <div className="quality__more--module-1">
                 <img src={Cloudy} alt="" className="forecast__img22" />
                 <div className="quality__more--container22">
-                  <h1>High: <span className="less__bold">100°C</span></h1>
-                  <h1>Low: <span className="less__bold">100°C</span></h1>
+                  <h1>
+                    High: <span className="less__bold">{Math.round(activeDay.day.maxtemp_f)}°F</span>
+                  </h1>
+                  <h1>
+                    Low: <span className="less__bold">{Math.round(activeDay.day.mintemp_f)}°F</span>
+                  </h1>
                 </div>
               </div>
               <div className="quality__more--module-1">
                 <div className="forecast__humid2">
                   <img src={Humid} alt="" />
-                  <h1 className="humid2__data">100%</h1>
+                  <h1 className="humid2__data">{activeDay.day.avghumidity}</h1>
                 </div>
                 <div className="forecast__humid2">
                   <img src={Rainy2} alt="" />
-                  <h1 className="humid2__data">100%</h1>
+                  <h1 className="humid2__data">{activeDay.day.daily_chance_of_rain}%</h1>
                 </div>
               </div>
               <div className="quality__more--module-1">
                 <div className="forecast__condition--container">
-                  <h1 className="forecast__condition--h1">UV: <span className="less__bold">0.7</span></h1>
-                  <h1 className="forecast__condition--h1">CO: <span className="less__bold">370</span></h1>
-                  <h1 className="forecast__condition--h1">NO2: <span className="less__bold">55</span></h1>
-                  <h1 className="forecast__condition--h1">O3: <span className="less__bold">29</span></h1>
-                  <h1 className="forecast__condition--h1">SO2: <span className="less__bold">18</span></h1>
+                  <h1 className="forecast__condition--h1">
+                    UV: <span className="less__bold">{activeDay.day.uv}</span>
+                  </h1>
+                  <h1 className="forecast__condition--h1">
+                    CO: <span className="less__bold">{Math.round(activeDay.day.air_quality.co)}</span>
+                  </h1>
+                  <h1 className="forecast__condition--h1">
+                    NO2: <span className="less__bold">{Math.round(activeDay.day.air_quality.no2)}</span>
+                  </h1>
+                  <h1 className="forecast__condition--h1">
+                    O3: <span className="less__bold">{Math.round(activeDay.day.air_quality.o3)}</span>
+                  </h1>
+                  <h1 className="forecast__condition--h1">
+                    SO2: <span className="less__bold">{Math.round(activeDay.day.air_quality.so2)}</span>
+                  </h1>
                 </div>
               </div>
               <div className="quality__more--module-1">
                 <div className="forecast__humid2">
                   <img src={Sunrise} alt="" />
-                  <h1 className="humid2__data">06:43</h1>
+                  <h1 className="humid2__data2">{activeDay.astro.sunrise.slice(0,5)}</h1>
                 </div>
                 <div className="forecast__humid2">
                   <img src={Sunset} alt="" />
-                  <h1 className="humid2__data">06:43</h1>
+                  <h1 className="humid2__data2">{activeDay.astro.sunset.slice(0,5)}</h1>
                 </div>
               </div>
             </div>
             <div className="quality__more--module-bottom">
               <div className="quality__more--module-2">
                 <div className="forecast__hourly--container">
-                  <div className="forecast__hourly">
-                    <h1 className="forecast__hourly--title">10:00</h1>
+                  {
+                    activeDay.hour.map((hour, index) => {
+                      return <div className="forecast__hourly" key={index}>
+                    <h1 className="forecast__hourly--title">{hour.time.slice(11)}</h1>
                     <div className="forecast__hourly--data">
                       <img src={Cloudy} alt="" className="hourly__img" />
-                      <h1>Partly Cloudy</h1>
-                      <h1>100°C</h1>
+                      <h1>{hour.condition.text}</h1>
+                      <h1>{Math.round(hour.temp_f)}°F</h1>
                       <div className="hourly__inner"></div>
                     </div>
                   </div>
-                  <div className="forecast__hourly">
-                    <h1 className="forecast__hourly--title">10:00</h1>
-                    <div className="forecast__hourly--data">
-                      <img src={Cloudy} alt="" className="hourly__img" />
-                      <h1>Partly Cloudy</h1>
-                      <h1>100°C</h1>
-                      <div className="hourly__inner"></div>
-                    </div>
-                  </div>
-                  <div className="forecast__hourly">
-                    <h1 className="forecast__hourly--title">10:00</h1>
-                    <div className="forecast__hourly--data">
-                      <img src={Cloudy} alt="" className="hourly__img" />
-                      <h1>Partly Cloudy</h1>
-                      <h1>100°C</h1>
-                      <div className="hourly__inner"></div>
-                    </div>
-                  </div>
-                  <div className="forecast__hourly">
-                    <h1 className="forecast__hourly--title">10:00</h1>
-                    <div className="forecast__hourly--data">
-                      <img src={Cloudy} alt="" className="hourly__img" />
-                      <h1>Partly Cloudy</h1>
-                      <h1>100°C</h1>
-                      <div className="hourly__inner"></div>
-                    </div>
-                  </div>
-                  <div className="forecast__hourly">
-                    <h1 className="forecast__hourly--title">10:00</h1>
-                    <div className="forecast__hourly--data">
-                      <img src={Cloudy} alt="" className="hourly__img" />
-                      <h1>Partly Cloudy</h1>
-                      <h1>100°C</h1>
-                      <div className="hourly__inner"></div>
-                    </div>
-                  </div>
-                  <div className="forecast__hourly">
-                    <h1 className="forecast__hourly--title">10:00</h1>
-                    <div className="forecast__hourly--data">
-                      <img src={Cloudy} alt="" className="hourly__img" />
-                      <h1>Partly Cloudy</h1>
-                      <h1>100°C</h1>
-                      <div className="hourly__inner"></div>
-                    </div>
-                  </div>
-                  <div className="forecast__hourly">
-                    <h1 className="forecast__hourly--title">10:00</h1>
-                    <div className="forecast__hourly--data">
-                      <img src={Cloudy} alt="" className="hourly__img" />
-                      <h1>Partly Cloudy</h1>
-                      <h1>100°C</h1>
-                      <div className="hourly__inner"></div>
-                    </div>
-                  </div>
+                    })
+                  }
+                 
                 </div>
               </div>
             </div>
